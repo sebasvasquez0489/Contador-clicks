b078ce0ecebc2fe1bc3d141ddd82925f87ce9cc0
Sebastian Vasquez <sebitas0489@gmail.com>
2024-01-19 21:51:56 -0500

    Funcionalidad a componente Counter usando hook useState.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import "./App.css";
 import Button from "./components/Button";
 import Counter from "./components/Counter";
-
+import { useState } from "react";
 import logoPage from "./img/front.png";
 
 function App() {
+  //Implementación de Hooks-useState para counter
+  const [numClicks, setNumClicks] = useState(0);
+
   //Definimos funcion handleClick y resetCounter para el Button
   const handleClick = () => {
-    console.log("Click");
+    setNumClicks(numClicks + 1); //Llama la funcion y actuliza estado.
   };
 
   const resetCounter = () => {
-    console.log("Reset");
+    setNumClicks(0); //llama funcio asignamos el valor y actualiza el estado.
   };
 
   //Estructura para renderizar.
@@ -21,7 +24,7 @@ function App() {
         <img className="logo" src={logoPage} alt="logo" />
       </div>
       <div className="main-container">
-        <Counter numClicks="5" />
+        <Counter numClicks={numClicks} />
         <Button text="Click" isButtonClick={true} handleClick={handleClick} />
         <Button text="Reset" isButtonClick={false} handleClick={resetCounter} />
       </div>
